5b3478158f4645bbbea6879275af53f503536ba2
Ilya Akdemir <ilya.akdemir@berkeley.edu>
2022-08-22 03:51:24 -0700

    Finalized Lab 1
    Added settings.py, case.law's utils.py, data for pandas visualizations and wrote more explanatory text

---
+++ Lab 1/config/settings.example.py
@@ -0,0 +1,7 @@
+from config.settings_base import *
+
+##### EDIT BELOW
+# claim your key at https://case.law/user/details
+API_KEY = "ad286fefc9c30c31a261fd18b2ec651a044c4256"
+API_URL = "https://api.case.law"
+API_VERSION = "v1"
