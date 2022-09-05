99eb32598557969ce885d04131e8012573983925
Ilya Akdemir <ilya.akdemir@berkeley.edu>
2022-09-04 23:51:57 -0700

    lab 4

--- Lab 4/config/settings.example.py
+++
@@ -1,7 +0,0 @@
-from config.settings_base import *
-
-##### EDIT BELOW
-# claim your key at https://case.law/user/details
-API_KEY = "INSERT API KEY HERE"
-API_URL = "https://api.case.law"
-API_VERSION = "v1"
--- Lab 4/config/settings_base.py
+++ Lab 4/config/settings_base.py
@@ -5,5 +5,6 @@ DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data')
 API_URL = "https://api.case.law"
 API_VERSION = "v1"
 API_BULK_URL = API_URL + "/" + API_VERSION + "/" + "bulk"
+
 # claim your key at https://case.law/user/details
-API_KEY = "INSERT API KEY HERE"
+API_KEY = "ad286fefc9c30c31a261fd18b2ec651a044c4256"
